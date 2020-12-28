b9c773885c0ca9510c515f73731a46a20dd59f3a
popthatpixel <pixelpopja@gmail.com>
2020-12-28 05:00:13 -0500

    Add Scaffold and AppBar with Drawer. Updated page design

--- lib/Screens/About/SmallScreen.dart
+++ lib/Screens/About/SmallScreen.dart
@@ -1,95 +1,385 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pixelpop/Utility/Colors.dart';
-import 'package:pixelpop/Utility/TextStyles.dart';
 
 class AboutSmallScreen extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: Container(
+          decoration: BoxDecoration(color: MyColors.black),
+          child: ListView(
+            padding: EdgeInsets.only(
+              left: 30,
+            ),
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: FlatButton(
+                  child: Text('Home',
+                      style: GoogleFonts.hindGuntur(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: FlatButton(
+                  child: Text('About',
+                      style: GoogleFonts.hindGuntur(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/about');
+                  },
+                ),
+              ),
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: FlatButton(
+                  child: Text('Portfolio',
+                      style: GoogleFonts.hindGuntur(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/work');
+                  },
+                ),
+              ),
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: FlatButton(
+                  child: Text('Clients',
+                      style: GoogleFonts.hindGuntur(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/clients');
+                  },
+                ),
+              ),
+              SizedBox(height: 10),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: FlatButton(
+                    child: Text('Contact',
+                        style: GoogleFonts.hindGuntur(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white)),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/contact');
+                    },
+                  )),
+            ],
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: MyColors.green,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: InkWell(
+            child: Image(
+              image: AssetImage(
+                'assets/logos/logo.png',
+              ),
+              fit: BoxFit.contain,
+            ),
+            onTap: () {
+              Navigator.pushNamed(context, '/');
+            },
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              _scaffoldKey.currentState.openDrawer();
+            },
+          )
+        ],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage(
-                'assets/mobile/about_page_mobile.png',
+                'assets/desktop/about_page_desktop.png',
               ),
-              fit: BoxFit.cover)),
-      child: SingleChildScrollView(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'About',
-            style: MyTextStyles.large,
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 20),
-            child: Text(
-              'I work as a User Interface & User Experience designer creating thoughtful experiences with the combination of design, business and marketing. I’m passionate about building & designing thoughtful experiences to make sure your customers and users are satisfied when they’re using your products and services online.I also love documenting my journey and sharing it with the community to help others succeed and grow.',
-              style: MyTextStyles.paragraph_center,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50),
-            child: Text(
-              'See what happens when great projects meet great design.',
-              style: MyTextStyles.bold_paragraph,
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40.0),
-            child: MaterialButton(
-              color: Colors.white,
-              elevation: 0,
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-              child: Text(
-                'CONTACT US',
-                style: TextStyle(color: MyColors.green),
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/contact');
-              },
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(color: MyColors.black),
+              fit: BoxFit.cover),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+              left: 10.0, right: 10.0, top: 10.0, bottom: 20),
+          child: SingleChildScrollView(
+              child: Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(8)),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(
-                  height: 30,
-                ),
-                Image(
-                  image: AssetImage('assets/logos/logo.png'),
                   height: 50,
                 ),
-                SizedBox(
-                  height: 20,
-                ),
                 Text(
-                  'PixelPop',
-                  style: MyTextStyles.paragraph,
+                  'ABOUT',
+                  style: GoogleFonts.montserrat(
+                      fontSize: 80,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 50.0, horizontal: 10),
+                  child: Text(
+                    'I work as a UI/UX designer creating thoughtful experiences with the combination of design, business and marketing. I’m passionate about building & designing thoughtful experiences to make sure your customers and users are satisfied when they’re using your products and services online.I also love documenting my journey and sharing it with the community to help others succeed and grow.',
+                    style: GoogleFonts.hindGuntur(
+                        fontSize: 18, color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
                 SizedBox(
                   height: 20,
                 ),
-                Text('Freelance Product Designer',
-                    style: MyTextStyles.paragraph),
-                SizedBox(
-                  height: 20,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 300,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              'Skills',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.white,
+                            height: 8.0,
+                            thickness: 1.0,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'UX Design',
+                              style: GoogleFonts.hindGuntur(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'UI Design',
+                              style: GoogleFonts.hindGuntur(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'Web Development',
+                              style: GoogleFonts.hindGuntur(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'App Development',
+                              style: GoogleFonts.hindGuntur(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      width: 300,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              'Applications',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.white,
+                            height: 8.0,
+                            thickness: 1.0,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'Adobe Xd',
+                              style: GoogleFonts.hindGuntur(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'After Effects',
+                              style: GoogleFonts.hindGuntur(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'Photoshop',
+                              style: GoogleFonts.hindGuntur(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Text(
+                              'Final Cut Pro',
+                              style: GoogleFonts.hindGuntur(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Text('©2020 PixelPop', style: MyTextStyles.paragraph),
-                SizedBox(
-                  height: 20,
+                Padding(
+                  padding: const EdgeInsets.only(top: 70.0),
+                  child: Text(
+                    'See what happens when great projects meet great design.',
+                    style: GoogleFonts.montserrat(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40.0),
+                  child: MaterialButton(
+                    color: Colors.white,
+                    elevation: 0,
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                    child: Text(
+                      'CONTACT US',
+                      style: GoogleFonts.montserrat(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: MyColors.green),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/contact');
+                    },
+                  ),
+                ),
+                Divider(
+                  color: Colors.white,
+                  height: 8.0,
+                  thickness: 1.0,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  // decoration: BoxDecoration(color: MyColors.black),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Image(
+                        image: AssetImage('assets/logos/logo.png'),
+                        height: 50,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'PixelPop',
+                        style: GoogleFonts.montserrat(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Freelance Product Designer',
+                        style: GoogleFonts.montserrat(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '©2020 PixelPop',
+                        style: GoogleFonts.montserrat(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
-      )),
+          )),
+        ),
+      ),
     );
   }
 }
