5867e79cd036c5bd400fbf6e45e181de9d437308
popthatpixel <pixelpopja@gmail.com>
2020-12-28 04:56:13 -0500

    Removed the app bar

--- lib/Screens/About/About.dart
+++ lib/Screens/About/About.dart
@@ -2,31 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:pixelpop/Screens/About/LargeScreen.dart';
 import 'package:pixelpop/Screens/About/MediumScreen.dart';
 import 'package:pixelpop/Screens/About/SmallScreen.dart';
-import 'package:pixelpop/Utility/Colors.dart';
 import 'package:pixelpop/Utility/Responsive_Utility.dart';
 
 class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: MyColors.green,
-        elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: InkWell(
-            child: Image(
-              image: AssetImage(
-                'assets/logos/logo.png',
-              ),
-              fit: BoxFit.contain,
-            ),
-            onTap: () {
-              Navigator.pushNamed(context, '/');
-            },
-          ),
-        ),
-      ),
       body: Container(
         child: ResponsiveUtility(
           largeScreen: AboutLargeScreen(),
