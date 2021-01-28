d9898949e5b2aa9a6fd554f663c8b7ea83ba258b
brainz876 <raheemavatar@gmail.com>
2021-01-28 18:41:22 -0500

    Added Firebase Analytics and Core

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,41 @@ import 'package:pixelpop/Screens/Clients/Clients.dart';
 import 'package:pixelpop/Screens/Contact/Contact.dart';
 import 'package:pixelpop/Screens/Home/Home.dart';
 import 'package:pixelpop/Screens/Work/Work.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
